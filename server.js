0a90aaae9e84d1744e8cc714908d347b90c1b01d
Soumyadip Samanta <samantasoumyadip1998@gmail.com>
2022-09-26 03:50:11 +0530

    cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -10,6 +11,14 @@ app.use(express.json());
 const connectDB = require('./config/db');
 //console.log('Failed here');
 connectDB();
+
+//Cors
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000','http://localhost:5000,'http://localhost:3300']
+} 
+
+app.use(cors(corsOptions));
 // template engine
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine','ejs');
